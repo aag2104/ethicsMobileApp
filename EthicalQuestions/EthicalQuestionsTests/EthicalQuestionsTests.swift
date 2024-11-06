fc380a9b3942f8c63ceb24d465ca45e7cc09a482
aag2104 <aag2104@tx.rr.com>
2024-11-06 14:55:50 -0600

    initial edits

---
+++ EthicalQuestions/EthicalQuestionsTests/EthicalQuestionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EthicalQuestionsTests.swift
+//  EthicalQuestionsTests
+//
+//  Created by Alexandra Geer on 11/6/24.
+//
+
+import Testing
+@testable import EthicalQuestions
+
+struct EthicalQuestionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
